e28e7f5bd4be0456d8c3da71f7eabb9693dbfffb
Arturoo09 <codealgr@gmail.com>
2023-03-31 12:02:27 +0200

    initial commit

---
+++ src/Components/Header/Top.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Top.css';
+
+const Top = () => {
+  return (
+    <div>
+        <div className="top">
+            
+            <div className="title">
+                <span>Portfolio.</span>
+            </div>
+
+            <nav class="navbar">
+                <ul>
+                    <li class="active">Home</li>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
+  )
+}
+
+export default Top
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import cv from './cv.pdf'
+import Typed from 'react-typed';
+import dog from '../../assets/dog.png';
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div>
+        <div class="home">
+
+            <div class="home-content">
+                <h3>Hello! , my name is</h3>
+                <h1>Arturo Largo García</h1>
+                <div className="typed-text">
+                    <Typed
+                        strings={["Computer Engineering Student",
+                                "Hacker",
+                                "FullStack Developer",
+                                "Tennis Enjoyer",
+                                "Human Being",]}
+                        typeSpeed={50}
+                        backSpeed={25}
+                        backDelay={1200}
+                        loop={true}
+                    />
+                </div>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque sit amet lorem sit amet mi euismod laoreet. Nulla eget erat leo </p>
+
+                <a href={cv} download>
+                    <button className="buttonCV">Download CV</button>
+                </a>
+            </div>
+
+            <div class="glass">
+                <div class="home-img">
+                    <img src={dog} alt="#"/>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Intro
